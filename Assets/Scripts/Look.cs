f389776cbac38243dedec2c18ebe46be0cd99a8d
JJ-ammaz <julian.jjammaz@gmail.com>
2026-04-07 19:04:05 -0400

    basic look script (DOES NOT FIT REQUIREMENTS ATM WILL RETWEAK WITH NEW INPUT SYSTEM ONPOINT)
    test script upload

---
+++ Assets/Scripts/Look.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class Look : MonoBehaviour
+{
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        Vector2 mouseWorldPos = Camera.main.ScreenToWorldPoint(Mouse.current.position.ReadValue());
+
+        Vector2 direction = mouseWorldPos - (Vector2)transform.position;
+
+        transform.up = direction;
+    }
+}

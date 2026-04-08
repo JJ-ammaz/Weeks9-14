1f87af95356b51150cc21c69971df6631715f3c3
JJ-ammaz <julian.jjammaz@gmail.com>
2026-04-08 12:09:56 -0400

    Changing name conventions in gun and adding prefabs and art, added rifle firing stuff. all instatiates are correctly named and such for bullets, redid rifle firing to fire over and over

--- Assets/Scripts/Gun.cs
+++ Assets/Scripts/Gun.cs
@@ -6,9 +6,9 @@ public class Gun : MonoBehaviour
     public GameObject singleBullet;
     public GameObject spreadBullet;
     public GameObject rapidBullet;
-    public bool PistolActive = false;
-    public bool ShotgunActive = false;
-    public bool RifleActive = false;
+    public bool SingleShotActive = false;
+    public bool SpreadShotActive = false;
+    public bool RapidShotActive = false;
     
     void Start()
     {
@@ -22,13 +22,18 @@ public class Gun : MonoBehaviour
 
     public void OnShoot(InputAction.CallbackContext context)
     {
+        if (RapidShotActive)
+        {
+            Instantiate(rapidBullet, transform.position, transform.rotation * Quaternion.Euler(0, 0, Random.Range(-2, 2)));
+        }
+
         if (context.performed == true)
         {
-            if (PistolActive)
+            if (SingleShotActive)
             {
                 Instantiate(singleBullet, transform.position, transform.rotation);
             }
-            if (ShotgunActive)
+            if (SpreadShotActive)
             {
                 Instantiate(spreadBullet, transform.position, transform.rotation);
                 Instantiate(spreadBullet, transform.position, transform.rotation * Quaternion.Euler(0, 0, Random.Range(-10, 10)));
@@ -43,10 +48,7 @@ public class Gun : MonoBehaviour
                 Instantiate(spreadBullet, transform.position, transform.rotation * Quaternion.Euler(0, 0, Random.Range(-10, 10)));
                 Instantiate(spreadBullet, transform.position, transform.rotation * Quaternion.Euler(0, 0, Random.Range(-10, 10)));
             }
-            if (RifleActive)
-            {
 
-            }
         }
     }
 }

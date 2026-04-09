36e0f4f9eeb492a277e3eb02eb7baed5eec02031
JJ-ammaz <julian.jjammaz@gmail.com>
2026-04-08 22:00:39 -0400

    fixed the "issues" with looking and is now using the in scope input system

--- Assets/Scripts/Look.cs
+++ Assets/Scripts/Look.cs
@@ -3,6 +3,9 @@ using UnityEngine.InputSystem;
 
 public class Look : MonoBehaviour
 {
+    private Vector2 mouseWorldPos;
+
+
     void Start()
     {
         
@@ -10,10 +13,18 @@ public class Look : MonoBehaviour
 
     void Update()
     {
-        Vector2 mouseWorldPos = Camera.main.ScreenToWorldPoint(Mouse.current.position.ReadValue());
-
+        
         Vector2 direction = mouseWorldPos - (Vector2)transform.position;
 
-        transform.right = direction;
+        transform.up = direction;
+        //so its looking the right way
+        transform.Rotate(0, 0, 90); 
+    }
+
+    public void OnPoint(InputAction.CallbackContext context)
+    {
+        //big helps to this https://slate.sheridancollege.ca/d2l/le/content/1461390/viewContent/18777907/View
+        // the lign basically is pulling the context from the ui pointer instead of how we usually do it and the camer.main.screenworld is in refrence to where the mouse is on the "camera"
+        mouseWorldPos = Camera.main.ScreenToWorldPoint(context.ReadValue<Vector2>());
     }
 }
